3530c9deb6f8cad42b15faea77e988838e2249f4
Dinesh <dinezk333@gmail.com>
2026-08-06 21:34:17 +0530

    Folder structure created and installed dependencies

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+export default function ComponentName() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Hero Section
+      </h1>
+    </section>
+  );
+}
